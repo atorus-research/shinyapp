8dad6150d2582a2d845ee4ddafb040b56566c2aa
Michael Stackhouse <michaelstackhouse0614@gmail.com>
2024-11-06 12:05:50 -0500

    Update R/app_ui.R
    Co-authored-by: Frank Menius <frank.menius@atorusresearch.com>

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,7 +10,7 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
-      h1("Hello!"),
+      h1("Hello!  YAY!"),
       h2("Welcome to my app")
     )
   )
